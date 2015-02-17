6f7e115e57f2d8237ccc689311ed53556e611b3b
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-02-17 09:29:13 +0100

    Bump version

--- xbm-life.el
+++ xbm-life.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/xbm-life
-;; Version: 0.0.2
+;; Version: 0.0.3
 ;; Package-Requires:
 ;; Keywords: games
 
