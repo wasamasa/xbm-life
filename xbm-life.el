0300004a6ea82f434bc23079bef31ad123195b9b
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-12-09 09:19:18 +0100

    Introduce a face for deciding fore- and background
    This should make it a bit more resilient to theme changes and allows you
    to theme it as well.

--- xbm-life.el
+++ xbm-life.el
@@ -38,19 +38,12 @@
   :group 'games
   :prefix "xbm-life-")
 
-(defcustom xbm-life-default-foreground (face-foreground 'default)
-  "Default foreground color of the grid."
-  :type 'string
-  :group 'xbm-life)
+(defface xbm-life '((t :inherit default))
+  "Used to determine the fore- and background color of the grid.")
 
 (defvar xbm-life-foreground nil
   "Current foreground color of the grid.")
 (make-variable-buffer-local 'xbm-life-foreground)
-
-(defcustom xbm-life-default-background (face-background 'default)
-  "Default background color of the grid."
-  :type 'string
-  :group 'xbm-life)
 
 (defvar xbm-life-background nil
   "Current background color of the grid.")
@@ -384,8 +377,8 @@ values like 0.01s."
   "Initialize demo."
   (interactive)
   (buffer-disable-undo)
-  (setq xbm-life-foreground xbm-life-default-foreground
-        xbm-life-background xbm-life-default-background
+  (setq xbm-life-foreground (face-foreground 'xbm-life nil t)
+        xbm-life-background (face-background 'xbm-life nil t)
         xbm-life-grid (xbm-life-create-empty-grid xbm-life-default-grid-size)
         xbm-life-grid-size (length xbm-life-grid)
         xbm-life-tile-size xbm-life-default-tile-size
